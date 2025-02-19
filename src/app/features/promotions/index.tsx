6195eaaa00971f3f52b839040c5fd39f3f607464
Lukas Raila <lukas.raila@zenitech.co.uk>
2025-02-19 11:54:31 +0200

    Remove comment

--- src/app/features/promotions/index.tsx
+++ src/app/features/promotions/index.tsx
@@ -8,13 +8,6 @@ type PromotionCardsProps = {
 };
 
 function PromotionCards({ numberOfCards }: PromotionCardsProps) {
-  // return card items `numberOfCards` times
-  //     <Card imageUrl="/placeholder.png">
-  //     <CardTitle text="Lotto offer" />
-  //     <CardContent content="Stake refunded as free bet if your selection finishes 2nd to the starting price favourite at the selected meetings. Max refund 20 per customer as a free bet." />
-  //     <Button fullWidth>More Info</Button>
-  //   </Card>
-
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-4">
